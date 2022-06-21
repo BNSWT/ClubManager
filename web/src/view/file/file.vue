cb0a20235f1e3b6c2b81af71d54520785ee3f5cc
yuyangz <zhouyuyang712@outlook.com>
2022-06-21 23:47:41 +0800

    Add file download support to file manger

--- web/src/view/file/file.vue
+++ web/src/view/file/file.vue
@@ -14,6 +14,8 @@
         <el-table-column align="right" label="">
           <template #default="scope">
             <el-button
+              v-if="scope.row.realPath ? true : false"
+              disabled
               icon="plus"
               size="small"
               type="primary"
@@ -21,10 +23,29 @@
               @click="openDialog(scope.row.ID)"
             >新增子文件/目录</el-button>
             <el-button
+              v-if="scope.row.realPath ? false : true"
+              icon="plus"
+              size="small"
+              type="primary"
+              link
+              @click="openDialog(scope.row.ID)"
+            >新增子文件/目录</el-button>
+            <el-button
+              v-if="scope.row.realPath ? false : true"
+              disabled
               icon="edit"
               size="small"
               type="primary"
               link
+              @click="openLink(scope.row.name)"
+            >下载文件</el-button>
+            <el-button
+              v-if="scope.row.realPath ? true : false"
+              icon="edit"
+              size="small"
+              type="primary"
+              link
+              @click="openLink(scope.row.name)"
             >下载文件/目录</el-button>
             <el-button
               icon="delete"
@@ -37,13 +58,36 @@
         </el-table-column>
       </el-table>
     </div>
-    <el-dialog v-model="dialogFormVisible" :before-close="closeDialog" title="弹窗操作">
+    <el-dialog v-model="dialogFormVisible" :before-close="closeDialog" title="新增文件/目录">
       <el-form :model="formData" label-position="right" label-width="80px">
         <el-form-item label="名称:">
           <el-input v-model="formData.name" clearable placeholder="请输入" />
         </el-form-item>
-        <el-form-item label="真实路径:">
-          <el-input v-model="formData.realPath" clearable placeholder="请输入" />
+        <el-form-item label="">
+          <form id="fromCont" method="post">
+            <div class="fileUpload" @click="inputChange">
+              选择附件
+              <input v-show="false" id="file" ref="FileInput" multiple="multiple" type="file" class="narrow-input" @change="choseFile"/>
+            </div>
+          </form>
+          <el-button :disabled="limitFileSize" type="primary" size="small" class="uploadBtn" @click="getFile">上传附件</el-button>
+        </el-form-item>
+        <el-form-item label="">
+          <div class="el-upload__tip">请上传不超过5MB的文件</div>
+        </el-form-item>
+        <el-form-item label="">
+          <div class="list">
+            <transition name="list" tag="p">
+              <div v-if="file">
+                <el-icon>
+                  <document />
+                </el-icon>
+                <span>{{ file.name }}</span>
+                <span class="percentage" align="right">{{ percentage }}%</span>
+                <el-progress :show-text="false" :text-inside="false" :stroke-width="2" :percentage="percentage" />
+              </div>
+            </transition>
+          </div>
         </el-form-item>
       </el-form>
       <template #footer>
@@ -64,6 +108,13 @@ export default {
 
 <script setup>
 import {
+  findFile as findBKFile,
+  breakpointContinueFinish,
+  removeChunk,
+  breakpointContinue
+} from '@/api/breakpoint'
+
+import {
   createFile,
   deleteFile,
   deleteFileByIds,
@@ -75,7 +126,8 @@ import {
 // 全量引入格式化工具 请按需保留
 import { getDictFunc, formatDate, formatBoolean, filterDict } from '@/utils/format'
 import { ElMessage, ElMessageBox } from 'element-plus'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
+import SparkMD5 from 'spark-md5'
 
 // 自动化生成的字典（可能为空）以及字段
 const formData = ref({
@@ -91,6 +143,15 @@ const pageSize = ref(10)
 const displayData = ref([])
 const tableData = ref([])
 const searchInfo = ref({})
+
+const file = ref(null)
+const fileMd5 = ref('')
+const formDataList = ref([])
+const waitUpLoad = ref([])
+const waitNum = ref(NaN)
+const limitFileSize = ref(false)
+const percentage = ref(0)
+const percentageFlage = ref(true)
 
 // 重置
 const onReset = () => {
@@ -235,6 +296,10 @@ const deleteFileFunc = async(row) => {
   }
 }
 
+const openLink = (fileName) => {
+  window.open('https://github.com/BNSWT/ClubManager/raw/main/server/fileDir/' + fileName)
+}
+
 // 弹窗控制标记
 const dialogFormVisible = ref(false)
 
@@ -281,7 +346,162 @@ const enterDialog = async() => {
     getTableData()
   }
 }
+
+// 选中文件的函数
+const choseFile = async(e) => {
+  const fileR = new FileReader() // 创建一个reader用来读取文件流
+  const fileInput = e.target.files[0] // 获取当前文件
+  const maxSize = 5 * 1024 * 1024
+  file.value = fileInput // file 丢全局方便后面用 可以改进为func传参形式
+  percentage.value = 0
+  if (file.value.size < maxSize) {
+    fileR.readAsArrayBuffer(file.value) // 把文件读成ArrayBuffer  主要为了保持跟后端的流一致
+    fileR.onload = async e => {
+      // 读成arrayBuffer的回调 e 为方法自带参数 相当于 dom的e 流存在e.target.result 中
+      const blob = e.target.result
+      const spark = new SparkMD5.ArrayBuffer() // 创建md5制造工具 （md5用于检测文件一致性 这里不懂就打电话问我）
+      spark.append(blob) // 文件流丢进工具
+      fileMd5.value = spark.end() // 工具结束 产生一个a 总文件的md5
+      const FileSliceCap = 1 * 1024 * 1024 // 分片字节数
+      let start = 0 // 定义分片开始切的地方
+      let end = 0 // 每片结束切的地方a
+      let i = 0 // 第几片
+      formDataList.value = [] // 分片存储的一个池子 丢全局
+      while (end < file.value.size) {
+        // 当结尾数字大于文件总size的时候 结束切片
+        start = i * FileSliceCap // 计算每片开始位置
+        end = (i + 1) * FileSliceCap // 计算每片结束位置
+        var fileSlice = file.value.slice(start, end) // 开始切  file.slice 为 h5方法 对文件切片 参数为 起止字节数
+        const formData = new window.FormData() // 创建FormData用于存储传给后端的信息
+        formData.append('fileMd5', fileMd5.value) // 存储总文件的Md5 让后端知道自己是谁的切片
+        formData.append('file', fileSlice) // 当前的切片
+        formData.append('chunkNumber', i) // 当前是第几片
+        formData.append('fileName', file.value.name) // 当前文件的文件名 用于后端文件切片的命名  formData.appen 为 formData对象添加参数的方法
+        formDataList.value.push({ key: i, formData }) // 把当前切片信息 自己是第几片 存入我们方才准备好的池子
+        i++
+      }
+      const params = {
+        fileName: file.value.name,
+        fileMd5: fileMd5.value,
+        chunkTotal: formDataList.value.length
+      }
+      const res = await findBKFile(params)
+      // 全部切完以后 发一个请求给后端 拉当前文件后台存储的切片信息 用于检测有多少上传成功的切片
+      const finishList = res.data.file.ExaFileChunk // 上传成功的切片
+      const IsFinish = res.data.file.IsFinish // 是否是同文件不同命 （文件md5相同 文件名不同 则默认是同一个文件但是不同文件名 此时后台数据库只需要拷贝一下数据库文件即可 不需要上传文件 即秒传功能）
+      if (!IsFinish) {
+        // 当是断点续传时候
+        waitUpLoad.value = formDataList.value.filter(all => {
+          return !(
+              finishList &&
+              finishList.some(fi => fi.FileChunkNumber === all.key)
+          ) // 找出需要上传的切片
+        })
+      } else {
+        waitUpLoad.value = [] // 秒传则没有需要上传的切片
+        ElMessage.success('文件已秒传')
+      }
+      waitNum.value = waitUpLoad.value.length // 记录长度用于百分比展示
+    }
+  } else {
+    limitFileSize.value = true
+    ElMessage('请上传小于5M文件')
+  }
+}
+
+const getFile = () => {
+  // 确定按钮
+  if (file.value === null) {
+    ElMessage('请先上传文件')
+    return
+  }
+  if (percentage.value === 100) {
+    percentageFlage.value = false
+  }
+  formData.value.name = file.value.name
+  formData.value.realPath = file.value.name
+  sliceFile() // 上传切片
+}
+
+const sliceFile = () => {
+  waitUpLoad.value &&
+  waitUpLoad.value.forEach(item => {
+    // 需要上传的切片
+    item.formData.append('chunkTotal', formDataList.value.length) // 切片总数携带给后台 总有用的
+    const fileR = new FileReader() // 功能同上
+    const fileF = item.formData.get('file')
+    fileR.readAsArrayBuffer(fileF)
+    fileR.onload = e => {
+      const spark = new SparkMD5.ArrayBuffer()
+      spark.append(e.target.result)
+      item.formData.append('chunkMd5', spark.end()) // 获取当前切片md5 后端用于验证切片完整性
+      upLoadFileSlice(item)
+    }
+  })
+}
+
+watch(() => waitNum.value, () => { percentage.value = Math.floor(((formDataList.value.length - waitNum.value) / formDataList.value.length) * 100) })
+
+const upLoadFileSlice = async(item) => {
+  // 切片上传
+  const fileRe = await breakpointContinue(item.formData)
+  if (fileRe.code !== 0) {
+    return
+  }
+  waitNum.value-- // 百分数增加
+  if (waitNum.value === 0) {
+    // 切片传完以后 合成文件
+    const params = {
+      fileName: file.value.name,
+      fileMd5: fileMd5.value
+    }
+    const res = await breakpointContinueFinish(params)
+    if (res.code === 0) {
+      // 合成文件过后 删除缓存切片
+      const params = {
+        fileName: file.value.name,
+        fileMd5: fileMd5.value,
+        filePath: res.data.filePath,
+      }
+      ElMessage.success('上传成功')
+      await removeChunk(params)
+    }
+  }
+}
+
+const FileInput = ref(null)
+const inputChange = () => {
+  FileInput.value.dispatchEvent(new MouseEvent('click'))
+}
+
 </script>
 
 <style>
+.fileUpload {
+  padding: 3px 10px;
+  font-size: 12px;
+  height: 20px;
+  line-height: 20px;
+  position: relative;
+  cursor: pointer;
+  color: #000;
+  border: 1px solid #c1c1c1;
+  border-radius: 4px;
+  overflow: hidden;
+  display: inline-block;
+}
+  narrow-input{
+    position: absolute;
+    font-size: 100px;
+    right: 0;
+    top: 0;
+    opacity: 0;
+    cursor: pointer;
+  }
+  .uploadBtn{
+    position: relative;
+    top: -10px;
+    margin-left: 15px;
+  }
+
 </style>
